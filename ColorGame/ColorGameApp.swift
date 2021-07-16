6feccb3b8c50e74d7b4adbe91b0b792350803b55
HuxleyMc <huxhales@gmail.com>
2021-07-16 22:08:08 +1000

    Initial Commit

---
+++ ColorGame/ColorGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorGameApp.swift
+//  ColorGame
+//
+//  Created by Huxley McGuffin on 16/7/21.
+//
+
+import SwiftUI
+
+@main
+struct ColorGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
